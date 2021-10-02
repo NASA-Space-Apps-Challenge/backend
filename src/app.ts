b79978e9f9e43955c4f4ab215817efd7a1fa0b54
Sebastian Marines <sebastian0marines@gmail.com>
2021-10-02 09:20:59 -0500

    Configure dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express, { Application, Request, Response } from 'express'
+import dotenv from 'dotenv'
 
 const app: Application = express()
 
-const port: number = 3001
+dotenv.config()
+const port: number = process.env.APP_PORT ? parseInt(process.env.APP_PORT) : 3000
 
 app.get('/toto', (req: Request, res: Response) => {
     res.send('Hello toto')
