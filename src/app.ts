723a9be68cd128ac7e5fb0d90c70c5449e17dcef
Sebastian Marines <sebastian0marines@gmail.com>
2021-10-02 20:45:55 -0500

    Enable CORS

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { Application, Request, Response } from 'express'
 import dotenv from 'dotenv'
 import axios from 'axios';
+import cors from 'cors'
 
 
 
 const app: Application = express()
+app.use(cors())
 
 dotenv.config()
 const port: number = process.env.APP_PORT ? parseInt(process.env.APP_PORT) : 3000
@@ -21,7 +23,8 @@ app.get("/", async (_req: Request, res: Response) => {
             x[0] = x[0].trim()
             return x
         })
-    console.info(clean_data[0])
+        .filter(arr => arr.length === 3)
+    console.info(clean_data.slice(-2))
     res.send(clean_data)
 })
 
